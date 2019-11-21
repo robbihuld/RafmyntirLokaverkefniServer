d4bb3661e2004a44fb84e07e5bf0a1bdc266a272
Róbert Ingi Huldarsson <rih4@hi.is>
2019-11-21 17:39:43 +0000

    env port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const Client = require('bitcoin-core');
 const express = require('express');
 const app = express();
 const cors = require('cors')
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+
 
 //Smileycoin
 const client = new Client({host: '192.168.1.109', port: '22001', username: 'robert', password: 'roberterbestur'})
@@ -29,6 +31,6 @@ io.on('connect', ()=>{
 client.getInfo().then((help) => console.log(help)).catch(e => console.error(e));
 
 
-const server = http.listen(4000, ()=>{
+const server = http.listen(process.env.PORT || 4000, ()=>{
   console.log('jeboddy')
 })
