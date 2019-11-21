bc192a977d5b414f8b598fb17d098e9793740de5
Róbert Ingi Huldarsson <rih4@hi.is>
2019-11-21 18:19:22 +0000

    test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const io = require('socket.io')(http)
 
 
 //Smileycoin
-const client = new Client({host: '192.168.1.109', port: '22001', username: 'robert', password: 'roberterbestur'})
+const client = new Client({host: process.env.SMILEY_URL, port: process.env.SMILEY_PORT, username: process.env.SMILEY_USER, password: process.env.SMILEY_PASS})
 
 //Express
 
@@ -22,13 +22,13 @@ app.use(express.static(__dirname));
 io.on('connection', function(){
   console.log()
   console.log('Socket connection')
+  client.getInfo().then((help) => console.log(help)).catch(e => console.error(e));
 })
 
 io.on('connect', ()=>{
   console.log('Asdf')
 })
 
-client.getInfo().then((help) => console.log(help)).catch(e => console.error(e));
 
 
 const server = http.listen(process.env.PORT || 4000, ()=>{
