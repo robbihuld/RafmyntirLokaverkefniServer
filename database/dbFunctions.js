aed42db9c80f13f4f736e66092d274922343a633
Róbert Ingi Huldarsson <rih4@hi.is>
2019-11-23 19:35:30 +0000

    fkn await

--- database/dbFunctions.js
+++ database/dbFunctions.js
@@ -21,7 +21,7 @@ async function insertCurrentLocationDb(lat, long, username){
 }
 
 async function getAddressForUserDb(username){
-  const result = query('SELECT address FROM users WHERE username = $1', [username]);
+  const result = await query('SELECT address FROM users WHERE username = $1', [username]);
   return result.rows[0]
 }
 
